e089e1bf88499c348fa7ce021db80ee16da3ca7b
Jan Grodowski <jgrodowski@gmail.com>
2021-12-01 18:05:24 +0100

    day 1 part 2

--- 1/count_increases.rb
+++ 1/count_increases.rb
@@ -5,4 +5,14 @@ num_increases = 0
 input.drop(1).each_with_index do |measurement, idx|
   num_increases += 1 if measurement > input[idx]
 end
-puts "Answer #{num_increases}"
+puts "Answer 1 #{num_increases}"
+
+puts "Sliding window of 3 measurements"
+num_increases = 0
+prev_sum = nil
+input.each_cons(3) do |measurements|
+  new_sum = measurements.sum
+  num_increases += 1 if prev_sum && new_sum > prev_sum
+  prev_sum = new_sum
+end
+puts "Answer 2 #{num_increases}"
