0165e56b91b6a43faacd99baee83344ad1beb38b
Jan Grodowski <jgrodowski@gmail.com>
2021-12-21 18:00:11 +0000

    Day 21 @ Rainforest

---
+++ 21/dirac_dice.rb
@@ -0,0 +1,22 @@
+
+Player = Struct.new(:score, :position, :name)
+dice = Enumerator.new(1..100).cycle
+positions = (1..10).cycle
+
+players = []
+players << Player.new(0, 7, 1)
+players << Player.new(0, 5, 2)
+cycle_players = players.cycle
+iter = 0
+loop do
+    iter += 1
+    current_player = cycle_players.next
+
+    move = dice.next + dice.next + dice.next
+    current_player.position = positions.take(current_player.position + move).last
+    current_player.score += current_player.position
+
+    break if current_player.score >= 1000
+end
+
+puts players, iter
